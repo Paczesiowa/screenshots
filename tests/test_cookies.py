20f0911ab9fe3fc13db10dd708cd9b4a94af4d7b
Bartek Ćwikłowski <paczesiowa@gmail.com>
2014-03-16 21:37:57 +0100

    polish cookies tests

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -4,87 +4,157 @@ import unittest2
 
 from dom2img import _cookies
 
+import tests.utils as utils
 
-class CookiesTest(unittest2.TestCase):
 
-    def test_serialize_cookies(self):
-        fun = _cookies.serialize_cookies
-        self.assertEqual(fun({}), b'')
+class SerializeCookiesTest(utils.TestCase):
 
-        self.assertEqual(fun({b'foo': b'bar'}), b'foo=bar')
+    FUN = _cookies.serialize_cookies
 
-        self.assertEqual(fun({b'foo': b'bar', b'baz': b'quux'}),
-                         b'foo=bar;baz=quux')
+    def test_empty(self):
+        self._check_result(b'', {})
 
-    def test_parse_cookie_string(self):
-        fun = _cookies.parse_cookie_string
-        exc = argparse.ArgumentTypeError
+    def test_simple(self):
+        self._check_result(b'foo=bar', {b'foo': b'bar'})
 
-        self.assertEqual(fun(u''), {})
-        self.assertEqual(fun(u'test'), {})
-        self.assertEqual(fun(u'='), {b'': b''})
-        self.assertEqual(fun(u'foo=bar'), {b'foo': b'bar'})
-        self.assertEqual(fun(u'foo=bar=baz'), {b'foo': b'bar=baz'})
-        self.assertEqual(fun(u'foo=bar=baz===;quux=plonk'),
-                         {b'foo': b'bar=baz===', b'quux': b'plonk'})
-        self.assertEqual(fun(b'foo=bar=baz===;quux=plonk'),
-                         {b'foo': b'bar=baz===', b'quux': b'plonk'})
+    def test_complex(self):
+        self._check_result(b'foo=bar;baz=quux',
+                           {b'foo': b'bar', b'baz': b'quux'})
 
-        self.assertRaisesRegexp(exc, 'cookie_string must be an ascii-only ' +
-                                'unicode text or a byte string', fun, None)
 
-        self.assertRaisesRegexp(exc, 'unicode cookie_string must ' +
-                                'be ascii-only', fun, u'föö=bär')
+class ParseCookieStringTest(utils.TestCase):
 
-    def test_parse_serialize_cookies(self):
-        fun1 = _cookies.serialize_cookies
-        fun2 = _cookies.parse_cookie_string
+    FUN = _cookies.parse_cookie_string
+    EXC = argparse.ArgumentTypeError
 
-        self.assertEqual(b'foo=bar',
-                         fun1(fun2(u'foo=bar')))
+    def test_empty(self):
+        self._check_result({}, b'')
 
-        self.assertEqual(b'foo=bar&baz=quux',
-                         fun1(fun2(u'foo=bar&baz=quux')))
+    def test_no_equals_char(self):
+        self._check_result({}, u'test')
 
-        self.assertEqual(b'foo=bar=quux==',
-                         fun1(fun2(u'foo=bar=quux==')))
+    def test_single_equals_char(self):
+        self._check_result({b'': b''}, u'=')
 
-    def test_validate_cookies(self):
-        fun = _cookies.validate_cookies
-        exc = argparse.ArgumentTypeError
+    def test_simple_byte_string(self):
+        self._check_result({b'foo': b'bar'}, b'foo=bar')
 
-        self.assertEqual(fun({}), {})
-        self.assertEqual(fun({b'foo': b'bar'}), {b'foo': b'bar'})
-        self.assertEqual(fun({u'foo': u'bar'}), {b'foo': b'bar'})
-        self.assertEqual(fun({b'foo': b'bar', u'baz': u'quux'}),
-                         {b'foo': b'bar', b'baz': b'quux'})
+    def test_simple_unicode(self):
+        self._check_result({b'foo': b'bar'}, u'foo=bar')
 
-        self.assertRaisesRegexp(exc, 'cookies must be a dict',
-                                fun, [])
-        self.assertRaisesRegexp(exc, 'cookies key/values must be strings',
-                                fun, {u'foo': []})
-        self.assertRaisesRegexp(exc, 'cookies key/values must be strings',
-                                fun, {3: b'bar'})
-        self.assertRaisesRegexp(exc, 'cookies keys/values must be ascii-only',
-                                fun, {u'föö': u'bar'})
-        self.assertRaisesRegexp(exc, 'cookies keys/values must be ascii-only',
-                                fun, {u'foo': u'bär'})
-        self.assertRaisesRegexp(exc, "cookies keys cannot use '=' character",
-                                fun, {u'f=o': b'bar'})
-        self.assertRaisesRegexp(exc,
-                                "cookies keys/values cannot use ';' character",
-                                fun, {u'f;o': b'bar'})
-        self.assertRaisesRegexp(exc,
-                                "cookies keys/values cannot use ';' character",
-                                fun, {u'foo': b'b;r'})
+    def test_cookie_value_with_equals_char(self):
+        self._check_result({b'foo': b'bar=baz'}, u'foo=bar=baz')
 
-    def test_get_cookie_domain(self):
-        fun = _cookies.get_cookie_domain
-        self.assertEqual(b'google.com', fun(b'http://google.com'))
-        self.assertEqual(b'google.com', fun(b'http://google.com/'))
-        self.assertEqual(b'google.com', fun(b'http://google.com/something'))
-        self.assertEqual(b'google.com', fun(b'http://google.com/?key=val'))
-        self.assertEqual(b'google.com', fun(b'http://google.com:8000'))
-        result = fun(
-            b'http://google.com:7000/path/something?key=val&key2=val2')
-        self.assertEqual(result, b'google.com')
+    def test_complex_unicode(self):
+        self._check_result({b'foo': b'bar=baz===', b'quux': b'plonk'},
+                           u'foo=bar=baz===;quux=plonk')
+
+    def test_complex_byte_string(self):
+        self._check_result({b'foo': b'bar=baz===', b'quux': b'plonk'},
+                           b'foo=bar=baz===;quux=plonk')
+
+    def test_non_ascii_unicode(self):
+        self._check_exception(u'unicode cookie_string must be ascii-only',
+                              u'föö=bär')
+
+    def test_wrong_type(self):
+        err_msg = u'cookie_string must be an ascii-only ' + \
+            u'unicode text or a byte string'
+        self._check_exception(err_msg, None)
+
+
+def parse_and_serialize_cookies(input_):
+    return _cookies.serialize_cookies(_cookies.parse_cookie_string(input_))
+
+
+class ParseAndSerializeCookiesTest(utils.TestCase):
+
+    FUN = parse_and_serialize_cookies
+
+    def test_simple_byte_string(self):
+        self._check_result(b'foo=bar', b'foo=bar')
+
+    def test_simple_unicode(self):
+        self._check_result(b'foo=bar', u'foo=bar')
+
+    def test_complex_byte_string(self):
+        self._check_result(b'foo=bar&baz=quux', b'foo=bar&baz=quux')
+
+    def test_complex_unicode(self):
+        self._check_result(b'foo=bar&baz=quux', u'foo=bar&baz=quux')
+
+    def test_multiple_equals_characters_in_value(self):
+        self._check_result(b'foo=bar=quux==', b'foo=bar=quux==')
+
+
+class ValidateCookiesTest(utils.TestCase):
+
+    FUN = _cookies.validate_cookies
+    EXC = argparse.ArgumentTypeError
+
+    def test_empty(self):
+        self._check_result({}, {})
+
+    def test_simple_byte_string(self):
+        self._check_result({b'foo': b'bar'}, {b'foo': b'bar'})
+
+    def test_simple_unicode(self):
+        self._check_result({b'foo': b'bar'}, {u'foo': u'bar'})
+
+    def test_complex(self):
+        self._check_result({b'foo': b'bar', b'baz': b'quux'},
+                           {b'foo': b'bar', u'baz': u'quux'})
+
+    def test_wrong_type(self):
+        self._check_exception(u'cookies must be a dict', [])
+
+    def test_wrong_type_of_values(self):
+        self._check_exception(u'cookies key/values must be strings',
+                              {u'foo': []})
+
+    def test_wrong_type_of_keys(self):
+        self._check_exception(u'cookies key/values must be strings',
+                              {3: u'bar'})
+
+    def test_non_ascii_unicode_keys(self):
+        self._check_exception(u'cookies keys/values must be ascii-only',
+                              {u'föö': u'bar'})
+
+    def test_non_ascii_unicode_values(self):
+        self._check_exception(u'cookies keys/values must be ascii-only',
+                              {u'foo': u'bär'})
+
+    def test_keys_with_equals_char(self):
+        self._check_exception(u"cookies keys cannot use '=' character",
+                              {u'f=o': b'bar'})
+
+    def test_keys_with_semicolon(self):
+        self._check_exception(u"cookies keys/values cannot use ';' character",
+                              {u'f;o': b'bar'})
+
+    def test_values_with_semicolon(self):
+        self._check_exception(u"cookies keys/values cannot use ';' character",
+                              {u'foo': b'b;r'})
+
+
+class GetCookieDomainTest(utils.TestCase):
+    FUN = _cookies.get_cookie_domain
+
+    def test_simple(self):
+        self._check_result(b'example.com', b'http://example.com')
+
+    def test_with_ending_slash(self):
+        self._check_result(b'example.com', b'http://example.com/')
+
+    def test_with_path(self):
+        self._check_result(b'example.com', b'http://example.com/something')
+
+    def test_with_query_part(self):
+        self._check_result(b'example.com', b'http://example.com/?key=val')
+
+    def test_with_port(self):
+        self._check_result(b'example.com', b'http://example.com:8000')
+
+    def test_complex(self):
+        input_ = b'http://example.com:7000/path/something?key=val&key2=val2'
+        self._check_result(b'example.com', input_)
