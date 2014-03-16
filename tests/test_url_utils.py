11d508072287fc7b6e1058ac8c6c252ec5f6b01e
Bartek Ćwikłowski <paczesiowa@gmail.com>
2014-03-16 18:45:39 +0100

    polish url_utils tests

--- tests/test_url_utils.py
+++ tests/test_url_utils.py
@@ -5,55 +5,60 @@ import bs4
 from dom2img import _url_utils
 
 
-class UrlUtilsTest(unittest2.TestCase):
+class IsAbsoluteURLTest(unittest2.TestCase):
 
-    def assertEqualStr(self, a, b):
-        return self.assertEqual(str(a), str(b))
+    def is_abs(self, url):
+        self.assertTrue(_url_utils.is_absolute_url(url))
 
-    def test_is_absolute_url(self):
-        self.assertTrue(_url_utils.is_absolute_url(
-            b'http://example.com/something'))
-        self.assertTrue(_url_utils.is_absolute_url(
-            b'https://example.com/something/something_else.txt'))
-        self.assertTrue(_url_utils.is_absolute_url(
-            b'http://example.com/something/../something.txt'))
+    def is_not_abs(self, url):
+        self.assertFalse(_url_utils.is_absolute_url(url))
 
-        self.assertFalse(_url_utils.is_absolute_url(
-            b'/something.txt'))
-        self.assertFalse(_url_utils.is_absolute_url(
-            b'/../something.txt'))
-        self.assertFalse(_url_utils.is_absolute_url(
-            b'//something/../something.txt'))
+    def test_simple(self):
+        self.is_abs(b'http://example.com/')
 
-    def test_absolutize_urls(self):
+    def test_url_with_path(self):
+        self.is_abs(b'https://example.com/something/something_else.txt')
+
+    def test_url_with_path_using_parent_references(self):
+        self.is_abs(b'http://example.com/something/../something.txt')
+
+    def test_url_without_scheme(self):
+        self.is_not_abs(b'/something.txt')
+
+    def test_url_without_scheme_using_parent_references(self):
+        self.is_not_abs(b'/../something.txt')
+
+    def test_url_with_protocol_relative_scheme(self):
+        self.is_not_abs(b'//something/something.txt')
+
+
+class AbsolutizeURLsTest(unittest2.TestCase):
+
+    def check(self, html_in, html_out, tag, attribute):
         bs = bs4.BeautifulSoup
-        prefix = 'http://127.0.0.1:8000/something'
+        prefix = 'http://example.com/something'
+        doc_in = bs(html_in)
+        doc_out = bs(html_out)
+        result = _url_utils.absolutize_urls(doc_in, tag, attribute, prefix)
+        self.assertEqual(str(doc_out), str(result))
+        self.assertEqual(str(doc_in), str(doc_out))
 
-        # make sure we return and modify in place
-        doc = bs('<a href="something"></a>')
-        self.assertEqualStr(
-            doc, _url_utils.absolutize_urls(doc, b'a', b'href', prefix))
+    def test_simple(self):
+        self.check(b'<a href="something"></a>',
+                   b'<a href="http://example.com/something"></a>',
+                   b'a', b'href')
 
-        # make sure we don't touch other tags
-        doc = bs('<a href="something"></a>')
-        self.assertEqualStr(bs('<a href="something"></a>'),
-                            _url_utils.absolutize_urls(doc, b'b',
-                                                       b'href', prefix))
+    def test_other_tags_are_not_changed(self):
+        self.check(b'<a href="something"></a>',
+                   b'<a href="something"></a>',
+                   b'b', b'href')
 
-        # make sure we don't touch other attributes
-        doc = bs('<a href="something"></a>')
-        self.assertEqualStr(bs('<a href="something"></a>'),
-                            _url_utils.absolutize_urls(doc, b'a',
-                                                       b'src', prefix))
+    def test_other_attributes_are_not_changed(self):
+        self.check(b'<a href="something"></a>',
+                   b'<a href="something"></a>',
+                   b'a', b'src')
 
-        # make sure we don't touch absolute urls
-        doc = bs('<a href="http://example.com/"></a>')
-        self.assertEqualStr(bs('<a href="http://example.com/"></a>'),
-                            _url_utils.absolutize_urls(doc, b'a',
-                                                       b'href', prefix))
-
-        # make sure it works
-        doc = bs('<a href="test"></a>')
-        self.assertEqualStr(bs('<a href="http://127.0.0.1:8000/test"></a>'),
-                            _url_utils.absolutize_urls(doc, b'a',
-                                                       b'href', prefix))
+    def test_absolute_urls_are_not_changed(self):
+        self.check(b'<a href="http://example.com/"></a>',
+                   b'<a href="http://example.com/"></a>',
+                   b'a', b'href')
