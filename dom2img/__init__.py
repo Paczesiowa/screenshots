a6c620b77f92fffd03a0f5dc39c1912381cb9b48
Bartek Ćwikłowski <paczesiowa@gmail.com>
2014-03-24 00:37:37 +0100

    __all__ in __init__.py

--- dom2img/__init__.py
+++ dom2img/__init__.py
@@ -1,6 +1,11 @@
 import pkg_resources
+from dom2img import _dom2img
 
 try:
     __version__ = pkg_resources.require('dom2img')[0].version
 except pkg_resources.DistributionNotFound:
     __version__ = 'dev'
+
+
+dom2img = _dom2img.dom2img
+__all__ = ['dom2img']
